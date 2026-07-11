179a610dc8ccaf404a262e56580ca56981b11836
Santiagoroseroq <sroseroq@unal.edu.co>
2026-07-11 13:46:23 -0500

    Create transformaciones.jl

---
+++ Proyecto algebra lineal/src/transformaciones.jl
@@ -0,0 +1,19 @@
+module Transformaciones
+
+export rotacion, traslacion, escalado
+
+function rotacion(theta)
+    c = cos(theta)
+    s = sin(theta)
+    return [c -s 0; s c 0; 0 0 1]
+end
+
+function traslacion(tx, ty)
+    return [1 0 tx; 0 1 ty; 0 0 1]
+end
+
+function escalado(sx, sy)
+    return [sx 0 0; 0 sy 0; 0 0 1]
+end
+
+end
